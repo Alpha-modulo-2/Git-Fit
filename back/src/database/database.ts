5937560a0971496a0954cb5027e265364419cb29
Thiago <thiagooitaven@hotmail.com>
2023-07-14 14:38:50 -0300

    remove commented code and unused environment variables

--- back/src/database/database.ts
+++ back/src/database/database.ts
@@ -3,25 +3,6 @@
 
 const mongoose = require("mongoose")
 
-// Global Variables
-// export const collections: { users?: mongoDB.Collection } = {}
-
-// Initialize Connection
-// export async function connectToDatabase() {
-
-//     const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-
-//     await client.connect();
-
-//     const db: mongoDB.Db = client.db(process.env.DB_NAME);
-
-//     const usersCollection: mongoDB.Collection = db.collection(process.env.USERS_COLLECTION_NAME);
-
-//     collections.users = usersCollection;
-
-//     console.log(`Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`);
-// }
-
 export async function connectToDatabase() {
     await mongoose.connect(process.env.DB_CONN_STRING)
     console.log(`Successfully connected to database`);
