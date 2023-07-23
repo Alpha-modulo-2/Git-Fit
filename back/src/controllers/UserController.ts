63caab7b04486a4ecdbe909560cd5ac2ee83765b
Carolina <carolinaliberatomarreiro@gmail.com>
2023-07-23 14:25:35 -0300

    fix: validations

--- back/src/controllers/UserController.ts
+++ back/src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import UserValidator from "../validators/UserValidator";
 import { Request, Response } from "express";
 import UserService from "../services/UserServices";
 
@@ -15,11 +16,25 @@ export default class UserController {
 
     async insert(req: Request, res: Response) {
         try {
+            const validationErrors = UserValidator(req.body);
+
+            if (Object.keys(validationErrors).length > 0) {
+                res.status(400).json({
+                    error: true,
+                    message: validationErrors,
+                });
+                return;
+            }
+
             const result = await this.service.insert(req.body);
             return res.status(result.statusCode || 500).json(result.user || result.message);
         } catch (error: any) {
             console.log("Erro ao inserir a conta", error.message);
-            return res.status(500);
+            return res.status(500).json({
+                error: true,
+                statusCode: 500,
+                message: `Erro ao inserir a conta ${error.message}`
+            });
         }
     }
 
@@ -31,7 +46,11 @@ export default class UserController {
             return res.status(result.statusCode || 500).json(result.user || result.message);
         } catch (error: any) {
             console.log("Erro ao atualizar a conta", error.message);
-            return res.status(500);
+            return res.status(500).json({
+                error: true,
+                statusCode: 500,
+                message: `Erro ao inserir a conta ${error.message}`
+            });
         }
     }
 
@@ -41,19 +60,37 @@ export default class UserController {
             return res.status(result.statusCode || 500).json(result.user || result.message);
         } catch (error: any) {
             console.log("Erro no login", error.message);
-            return res.status(500);
+            return res.status(500).json({
+                error: true,
+                statusCode: 500,
+                message: `Erro no login ${error.message}`
+            });
         }
     }
 
     async update(req: Request, res: Response) {
         const { id } = req.params
 
+        const validationErrors = UserValidator(req.body);
+
+        if (Object.keys(validationErrors).length > 0) {
+            res.status(400).json({
+                error: true,
+                message: validationErrors,
+            });
+            return;
+        }
+
         try {
             const result = await this.service.update(id, req.body);
             return res.status(result.statusCode || 500).json(result.user || result.message);
         } catch (error: any) {
             console.log("Erro ao atualizar a conta 1", error);
-            return res.status(500);
+            return res.status(500).json({
+                error: true,
+                statusCode: 500,
+                message: `Erro ao atualizar a conta 1 ${error.message}`
+            });
         }
     }
 
@@ -65,8 +102,12 @@ export default class UserController {
             const result = await this.service.delete(id);
             return res.status(result.statusCode || 500).json("" || result.message);
         } catch (error: any) {
-            console.log("Erro ao atualizar a conta 1", error);
-            return res.status(500);
+            console.log("Erro ao deletar a conta", error);
+            return res.status(500).json({
+                error: true,
+                statusCode: 500,
+                message: `Erro ao deletar a conta ${error.message}`
+            });
         }
     }
 }
