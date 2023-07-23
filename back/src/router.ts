a0fd241896def83a344bd068ee3671252e4ce87e
Carolina <carolinaliberatomarreiro@gmail.com>
2023-07-23 14:23:22 -0300

    fix: comments

--- back/src/router.ts
+++ back/src/router.ts
@@ -16,14 +16,14 @@ router.delete("/users/:id", userController.delete);
 const cardController = new CardController();
 
 //Card Routes
-router.post("/cards/:userId", cardController.insert); //Retorna um array com os 7 cards criados
-router.get("/allcards/:userId", cardController.get) //Retorna um arrays com os 7 cards daquele user
-router.get("/card/:cardId", cardController.getOne) //Retorna somente 1 card de acordo com o id do card
-router.post("/card/:cardId/task", cardController.addTask); //Add uma task ao card indicado
-router.post("/card/:cardId/meal", cardController.addMeal); //Add uma refeição ao card indicado
-router.patch("/card/:cardId/task/:taskId", cardController.updateTask); //Atualiza uma task do card indicado
-router.patch("/card/:cardId/meal/:mealId", cardController.updateMeal); //Atualiza uma meal do card indicado
-router.delete("/card/:cardId/task/:taskId", cardController.delTask); //Deleta uma task do card indicado
-router.delete("/card/:cardId/meal/:mealId", cardController.delMeal); //Deleta uma meal do card indicado
+router.post("/cards/:userId", cardController.insert);
+router.get("/allcards/:userId", cardController.get);
+router.get("/card/:cardId", cardController.getOne);
+router.post("/card/:cardId/task", cardController.addTask); 
+router.post("/card/:cardId/meal", cardController.addMeal);
+router.patch("/card/:cardId/task/:taskId", cardController.updateTask);
+router.patch("/card/:cardId/meal/:mealId", cardController.updateMeal);
+router.delete("/card/:cardId/task/:taskId", cardController.delTask);
+router.delete("/card/:cardId/meal/:mealId", cardController.delMeal);
 
 export { router };
