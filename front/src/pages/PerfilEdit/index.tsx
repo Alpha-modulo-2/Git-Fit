7b1c6dcdfb80ade0fa882b4ef5399404bfab92b7
PedroMCarvalho <pedromcarvalho97@gmail.com>
2023-07-25 01:44:43 -0300

    test edit/delete users

--- front/src/pages/PerfilEdit/index.tsx
+++ front/src/pages/PerfilEdit/index.tsx
@@ -1,10 +1,11 @@
 import "./styles.css";
 import { Header } from "../../components/Header";
-import { NavigateFunction, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
+import { useParams } from "react-router-dom";
 
 export const PerfilEdit = () => {
-  const navigate: NavigateFunction = useNavigate();
+  const params = useParams();
+  alert(params.id);
   return (
     <div className="Edit">
       <Header isLoggedIn={true} />
@@ -18,13 +19,13 @@ export const PerfilEdit = () => {
                 <input type="submit" className="addPhoto-edit" value="Adicionar foto" />
               </form>
               <br />
-              <input type="text" className="input-edit" placeholder="Nome Completo" />
+              <input type="text" id="userName" className="input-edit" placeholder="Nome Completo" />
               <br />
-              <input type="text" className="input-edit" placeholder="E-mail" />
+              <input type="text" id="email" className="input-edit" placeholder="E-mail" />
               <br />
-              <input type="text" className="input-edit" placeholder="Idade" />
+              <input type="text" id="age" className="input-edit" placeholder="Idade" />
               <br />
-              <input type="text" className="input-edit" placeholder="Profissão" />
+              <input type="text" id="occupation" className="input-edit" placeholder="Profissão" />
             </div>
 
             <div className="container-second-content-edit">
@@ -35,7 +36,7 @@ export const PerfilEdit = () => {
                 <input type="text" className="input-weightHight-edit" placeholder="Altura" />
               </div>
               <br />
-              <input type="text" className="input-edit" placeholder="Senha" />
+              <input type="text" id="password"className="input-edit" placeholder="Senha" />
               <br />
               <input type="text" className="input-edit" placeholder="Confirme a senha" />
               <br />
@@ -52,10 +53,44 @@ export const PerfilEdit = () => {
 
           </div>
             <div className="divButton-edit">
-              <Button category="primary" label="Editar" onClick={() => navigate("/")} />
+              <Button category="primary" label="Editar" onClick={async () => {
+              let userName = (document.getElementById("userName") as HTMLInputElement)?.value;
+              let email = (document.getElementById('email') as HTMLInputElement)?.value;
+              let occupation = (document.getElementById('occupation') as HTMLInputElement)?.value;
+              let password = (document.getElementById('password') as HTMLInputElement)?.value;
+
+              // let passwordCheck = document.getElementById('passwordCheck')?.textContent;
+
+              let user = {userName,  email,  occupation, password};
+              console.log(user)
+              
+              const response = await fetch(`http://localhost:3000/users/${params.id}`, {
+                method: 'PATCH',
+                body: JSON.stringify(user),
+                headers: {'Content-Type': 'application/json'} 
+              });
+          
+              if (!response.ok) 
+              { 
+                  console.error("Error");
+              }
+ 
+            }} />
             </div>
             <div className="divDeleteAccount-edit">
-              <button className="deleteAccount-edit">Excluir Conta</button>
+              {/* <button className="deleteAccount-edit" onClick={()=> navigate("/")}>Excluir Conta</button> */}
+              <Button category="primary" label="Deletar Conta" onClick={async () => {
+
+              const response = await fetch(`http://localhost:3000/users/${params.id}`, {
+                method: 'DELETE',
+              });
+          
+              if (!response.ok) 
+              { 
+                  console.error("Error");
+              }
+ 
+            }} />
             </div>
           </div>
           
