28268cbf6fb52f517f4adc664d628102e9b38b53
Thiago <thiagooitaven@hotmail.com>
2023-07-25 09:21:13 -0300

    add auth middleware to user routes

--- back/src/middleware/authenticate.ts
+++ back/src/middleware/authenticate.ts
@@ -7,12 +7,17 @@ export default async function authenticate(
     next: NextFunction
 ) {
     try {
+        if (!process.env.JWTSECRET) {
+            throw new Error('JWTSECRET nao definido');
+        }
+
         jwtLib.verify(
             req.cookies["session"],
             process.env.JWTSECRET
         );
         next();
+
     } catch (error: any) {
-        res.status(401).redirect("/");
+        res.status(401).json({ errors: error.message });
     }
 }
