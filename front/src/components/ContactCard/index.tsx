d4b062643aa3d6c9fee8bae97e89896ae28327cc
Gabriela Rayla <grgabrielarayla@gmail.com>
2023-07-31 20:26:59 -0300

    connecting with backend

--- front/src/components/ContactCard/index.tsx
+++ front/src/components/ContactCard/index.tsx
@@ -2,29 +2,61 @@
 import React from 'react';
 import { XCircle, Check } from "@phosphor-icons/react";
 
-interface ContactsProps{
-    photo: string;
-    name: string;
-    occupation: string;
-    id: string;
+interface User {
+  _id: string;
+  userName: string;
+  password: string;
+  email: string;
+  friends: Friend[];
+  gender: string;
+  weight: string;
+  height: string;
+  occupation: string;
+  age: number;
+  created_at: string;
+  updated_at: string;
+  __v: number;
+}
+interface Friend {
+  _id: string;
+  userName: string;
+  password: string;
+  email: string;
+  friends: Friend[]; // Array de IDs dos amigos (pode ser string[] ou Friend[])
+  gender: string;
+  weight: string;
+  height: string;
+  occupation: string;
+  age: number;
+  created_at: string;
+  updated_at: string;
+  __v: number;
 }
 
 interface ContactCardProps {
-  user: ContactsProps;
+  requesterInfo?: User;
   onAddClick: (userId: string, friendId: string) => void;
   onRemoveClick?: (userId: string) => void;
 }
 
-const ContactCard: React.FC<ContactCardProps> = ({ user, onAddClick, onRemoveClick }) => {
+const ContactCard: React.FC<ContactCardProps> = ({ requesterInfo, onAddClick, onRemoveClick }) => {
   return (
     <div className="contact-card">
       <div className="img-card-contacts">
-        <img src={user.photo} alt="" />
+        <img src={'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg'} alt="" />
       </div>
 
       <div className="user-contacts-info">
-        <p className="username-contact">{user.name.length > 12 ? user.name.substring(0, 10) + "..." : user.name}</p>
-        <p className="useroccupation-contact">{user.occupation.length > 12 ? user.occupation.substring(0, 10) + "..." : user.occupation}</p>
+        <p className="username-contact">
+          {requesterInfo?.userName && requesterInfo.userName.length > 12 
+            ? requesterInfo.userName.substring(0, 10) + "..." 
+            : requesterInfo?.userName}
+        </p>
+        <p className="useroccupation-contact">
+          {requesterInfo?.occupation && requesterInfo?.occupation.length > 12 
+            ? requesterInfo.occupation.substring(0, 10) + "..." 
+            : requesterInfo?.occupation}
+        </p>
       </div>
       {onRemoveClick && (
             <div className="container-icon-contact">
@@ -32,13 +64,13 @@ const ContactCard: React.FC<ContactCardProps> = ({ user, onAddClick, onRemoveCli
                     size={20}
                     color="black"
                     className="icon-add-contact"
-                    onClick={() => onAddClick(user.id, 'friendId')}
+                    onClick={() =>  requesterInfo && onAddClick(requesterInfo._id, 'friendId')}
                 />
                 <XCircle
                     size={20}
                     color="black"
                     className="icon-remove-contact"
-                    onClick={() => onRemoveClick(user.id)}
+                    onClick={() =>  requesterInfo && onRemoveClick(requesterInfo._id)}
                 />
             </div>
         )}
--- front/src/pages/ContactsAndRequests/index.tsx
+++ front/src/pages/ContactsAndRequests/index.tsx
@@ -5,175 +5,138 @@ import { useState } from "react";
 import ContactCard from "../../components/ContactCard";
 import { Chat } from "../../components/Chat";
 
-interface ContactsProps{
-    photo: string;
-    name: string;
+interface FriendRequest {
+    _id: string;
+    requester: string;
+    recipient: string;
+    status: string;
+    created_at: string;
+    __v: number;
+    requesterInfo?: User
+  }
+  
+  interface ApiResponseRequests {
+    error: boolean;
+    statusCode: number;
+    friendRequests: FriendRequest[];
+  }
+
+  interface Friend {
+    _id: string;
+    userName: string;
+    password: string;
+    email: string;
+    friends: Friend[]; // Array de IDs dos amigos (pode ser string[] ou Friend[])
+    gender: string;
+    weight: string;
+    height: string;
     occupation: string;
-    id: string;
-}
+    age: number;
+    created_at: string;
+    updated_at: string;
+    __v: number;
+  }
+  
+  interface User {
+    _id: string;
+    userName: string;
+    password: string;
+    email: string;
+    friends: Friend[];
+    gender: string;
+    weight: string;
+    height: string;
+    occupation: string;
+    age: number;
+    created_at: string;
+    updated_at: string;
+    __v: number;
+  }
+
 
 
 export const Contacts = () => {
-    const [contacts, setContacts] = useState<ContactsProps[]>();
-    const [requests, setRequests] = useState<ContactsProps[]>([]);
+    const [contacts, setContacts] = useState<Friend[]>();
+    const [requests, setRequests] = useState<FriendRequest[] | null>(null);
     const [showRequests, setShowRequests] = useState(false); // Estado para controlar exibição das solicitações
-    // const [error, setError] = useState('');
+    const [error, setError] = useState('');
 
-    const getContacts =  () => {
-        try {
-            const users: ContactsProps[] = [
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'João da Silva',
-                occupation: 'Engenheiro',
-                id: '1'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Maria Oliveira',
-                occupation: 'Médica',
-                id: '2'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Pedro Santos',
-                occupation: 'Advogado',
-                id: '3'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Ana Souza',
-                occupation: 'Professor',
-                id: '4'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Lucas Lima',
-                occupation: 'Estudante',
-                id: '5'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Isabela Costa',
-                occupation: 'Arquiteta',
-                id: '6'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Ricardo Mendes da Silva',
-                occupation: 'Programador',
-                id: '7'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Fernanda Rodrigues',
-                occupation: 'Enfermeira',
-                id: '8'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Gabriel Alves',
-                occupation: 'Designer',
-                id: '9'
-            },
-            {
-                photo: 'https://1.bp.blogspot.com/-KLg5TEY1v6U/T6P9I6YPZwI/AAAAAAAABEc/iYpstw_ouMQ/s1600/Mr_bean.jpg',
-                name: 'Carolina Castro',
-                occupation: 'Psicóloga',
-                id: '10'
-            },];
-            setContacts(users)
-            // const response = await fetch('/contacts/list');
-            // if (response.ok) {
-                
-            //     const data = await response.json();
-            //     setContacts(data); 
-            // } else {
-            //     console.error('Erro ao obter os contatos');
-            //     setError('Failed to fetch contacts.');
-            // }
-        }catch (error) {
-            console.error('Erro na requisição:', error);
-            // setError('An error occurred while fetching the contacts.');
+    
+    
+    
+    const handleShowContacts = () => {
+        console.log('caiu')
+        getContacts().catch((error) => {
+            console.error('Erro ao obter as solicitações:', error);
+          });
+
+        async function getContacts() {
+            try {
+                const userId= 	'64bd4cc8e86446a01cb52ee7';
+    
+                    
+                const response = await fetch(`https://localhost:443/users/${userId}`);
+                if (response.ok) {
+                    const data = await response.json() as User;
+                    setContacts(data.friends); 
+    
+                    console.log(data.friends, 'friendss')
+                    return;
+                } else {
+                    console.error('Erro ao obter as solicitações');
+                    console.log(error)
+                    setError('Erro ao obter as solicitações.');
+                }
+            }catch (error) {
+                console.error('Erro na requisição:', error);
+                // setError('Erro na requisição');
+            }
         }
-    }
-    const getRequests =  () => {
-        try {
-            const users: ContactsProps[] = [
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Júlia Souza',
-                    occupation: 'Atriz',
-                    id: '1'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Ana Silva',
-                    occupation: 'Advogada',
-                    id: '2'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Luana Freitas',
-                    occupation: 'Nutricionista',
-                    id: '3'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Ana Souza',
-                    occupation: 'Professor',
-                    id: '4'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Lucas Lima',
-                    occupation: 'Estudante',
-                    id: '5'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Isabela Costa',
-                    occupation: 'Arquiteta',
-                    id: '6'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Ricardo Mendes da Silva',
-                    occupation: 'Programador',
-                    id: '7'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Fernanda Rodrigues',
-                    occupation: 'Enfermeira',
-                    id: '8'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Gabriel Alves',
-                    occupation: 'Designer',
-                    id: '9'
-                },
-                {
-                    photo: 'https://conteudo.imguol.com.br/c/esporte/26/2021/02/25/terry-crews-interpretou-julius-rock-em-todo-mundo-odeia-o-chris-1614287010913_v2_600x600.jpg',
-                    name: 'Carolina Castro',
-                    occupation: 'Psicóloga',
-                    id: '10'
-                },];
-                setRequests(users)
-            // const response = await fetch('/contacts/solicitations');
-            // if (response.ok) {
-            //     const data = await response.json();
-            //     setRequests(data); 
-            // } else {
-            //     console.error('Erro ao obter as solicitações');
-            //     setError('Erro ao obter as solicitações.');
-            // }
-        }catch (error) {
-            console.error('Erro na requisição:', error);
-            // setError('Erro na requisição');
+        setShowRequests(false);
+    };
+
+    const handleShowRequests = () => {
+        console.log('caiu')
+        getRequests().catch((error) => {
+            console.error('Erro ao obter as solicitações:', error);
+          });
+
+        async function getRequests() {
+            try {
+                const userId= 	'64bd4cc8e86446a01cb52ee7';
+    
+                    
+                const response = await fetch(`https://localhost:443/friendRequests/${userId}`);
+                if (response.ok) {
+                    const data = await response.json() as ApiResponseRequests;
+                    setRequests(data.friendRequests); 
+    
+                    // obtendo informações sobre cada requester
+                    const requesterIds = data.friendRequests.map((request) => request.requester);
+                    const requesterDetails = await Promise.all(requesterIds.map((requesterId) => fetch(`https://localhost:443/users/${requesterId}`)));
+                    const requesterData = await Promise.all(requesterDetails.map((response) => response.json()));
+                    
+                    // atualizando os friendRequests com as informações obtidas
+                    const updatedRequests:  FriendRequest[] = data.friendRequests.map((request, index) => ({
+                        ...request,
+                        requesterInfo: requesterData[index] as User, 
+                        // Adicionandp as informações do usuário em um campo chamado 'requesterInfo'
+                    }));
+                    
+                    setRequests(updatedRequests);
+                    return;
+                } else {
+                    console.error('Erro ao obter as solicitações');
+                    setError('Erro ao obter as solicitações.');
+                }
+            }catch (error) {
+                console.error('Erro na requisição:', error);
+                // setError('Erro na requisição');
+            }
         }
-    }
+        setShowRequests(true);
+    };
+    
     const updateFriends = (userId: string, friendId: string) => {
         console.log(userId, friendId)
         fetch(`/friends/${userId}`, {
@@ -201,11 +164,12 @@ export const Contacts = () => {
             // setError('An error occurred while updating the contacts.');
         });
     };
+
+   
     
-//       // useEffect com a função getContacts
+      // useEffect com a função getContacts
 //   useEffect(() => {
-//     getContacts();
-        // getRequests();
+//     getRequests();
     
 //   }, []);
 
@@ -226,34 +190,28 @@ export const Contacts = () => {
                 </div>
                 <div className="content-contacts">
                     <div className="container-titles-contacts">
-                        <p className={`title-contacts title-content-left ${!showRequests ? 'active' : ''}`} onClick={()=> {
-                            getContacts(); 
-                            setShowRequests(false)
-                        }}
+                        <p className={`title-contacts title-content-left ${!showRequests ? 'active' : ''}`} onClick={handleShowContacts}
                         >Contatos</p>
-                        <p className={`title-contacts title-content-right ${showRequests ? 'active' : ''}`} onClick={() => {
-                            getRequests();
-                            setShowRequests(true);
-                        }}             
+                        <p className={`title-contacts title-content-right ${showRequests ? 'active' : ''}`} onClick={handleShowRequests}             
                         >Solicitações</p>
                     </div>
                     <p className="contacts-line"></p>
 
                     {/* div dos cards */}
                     <div className="container-contact-cards">
-                    {showRequests ? requests.map((user) => (
+                    {showRequests ? requests?.map((requester) => (
                         <ContactCard
-                        key={user.id}
-                        user={user}
+                        key={requester._id}
+                        requesterInfo={requester.requesterInfo}
                         onAddClick={(userId, friendId) => updateFriends(userId, friendId)}
                         onRemoveClick={(userId) => console.log(userId)
                         } 
                         // logica para remover contato
                         />
-                    )) : contacts?.map((user) => (
+                    )) : contacts?.map((friend) => (
                         <ContactCard
-                        key={user.id}
-                        user={user}
+                        key={friend._id}
+                        requesterInfo={friend}
                         onAddClick={(userId, friendId) => updateFriends(userId, friendId)}
                         onRemoveClick={undefined}
                         />
