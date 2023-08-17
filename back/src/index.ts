4e7fbb31bc5fba1204e0bff2a0d6fe11459e1e40
Thiago <thiagooitaven@hotmail.com>
2023-08-17 09:29:26 -0300

    staging fix attempt

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,6 +3,7 @@ import { App } from "./app";
 import { connectToDatabase } from "./database/database";
 import fs from 'fs';
 import https from 'https'
+import http from 'http';
 import path from 'path';
 import { Server } from 'socket.io';
 import SocketController from './controllers/socketController';
@@ -19,16 +20,28 @@ try {
 
 const app = new App().server;
 
-const options = {
-    key: fs.readFileSync(path.resolve(process.env.SSL_KEY_PATH)),
-    cert: fs.readFileSync(path.resolve(process.env.SSL_CERT_PATH)),
-};
-const server = https.createServer(options, app);
+if (process.env.RENDER_PORT) {
+    const server = http.createServer(app);
+    const io = new Server(server, { cors: { origin: "*" } });
 
-const io = new Server(server, { cors: { origin: "*" } });
+    new SocketController(io);
 
-new SocketController(io);
+    server.listen(process.env.RENDER_PORT, () => {
+        console.log(`Server is running on port ${process.env.RENDER_PORT}`);
+    });
+} else {
+    const options = {
+        key: fs.readFileSync(path.resolve(process.env.SSL_KEY_PATH)),
+        cert: fs.readFileSync(path.resolve(process.env.SSL_CERT_PATH)),
+    };
+    const server = https.createServer(options, app)
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+    const io = new Server(server, { cors: { origin: "*" } });
+
+    new SocketController(io);
+
+    server.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}
+
