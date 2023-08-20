66f67c0cb8ed46f9244a46afb4b8b34ffab73c63
Thiago <thiagooitaven@hotmail.com>
2023-08-20 05:46:06 -0300

    fix login and perfilEdit

--- front/src/context/authContext.tsx
+++ front/src/context/authContext.tsx
@@ -44,7 +44,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  console.log('useAuth', context)
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
